76c24b783f756a4bebd53fe287377752d33795f1
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-05-20 20:23:00 +0200

    Update seed_crypto.py

--- core/seed_crypto.py
+++ core/seed_crypto.py
@@ -1,1 +1,9 @@
-
+# Copyright 2025: NemesisCyberForce
+# App: CryptGuardian 
+# File: CryptGuardian/core/seed_crypto.py
+# Version: 0.1
+# Comment: Core (Crypto Seed)
+# soon
+# load bc
+# load logic
+# oberve seed (optional blockchain)
