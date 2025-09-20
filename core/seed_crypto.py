1be984bbe25c0c534eb6208cda517f39b478926a
Volkan Şah <VolkanSah@users.noreply.github.com>
2025-09-21 00:51:02 +0200

    Delete core/seed_crypto.py

--- core/seed_crypto.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2025: NemesisCyberForce
-# App: CryptGuardian 
-# File: CryptGuardian/core/seed_crypto.py
-# Version: 0.1
-# Comment: Core (Crypto Seed)
-# soon
-# load bc
-# load logic
-# oberve seed (optional blockchain)
